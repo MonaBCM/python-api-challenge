e6edbaf906d3ad2aeef6828f14d9f439479441b6
MonaBCM <helinkanya@gmail.com>
2023-02-06 05:43:48 -0500

    add api keys

--- weatherpy/api_keys.py
+++ weatherpy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "620999904eae58038fac8d943ca70db4"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "d4b1a9a1217d48448d80965efc1a2c68"
